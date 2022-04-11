e4f9e7be63a821b0486110ddb208fa9c8a11f469
Dnyandeo bansode <bansodednyanadeo@gmail.com>
2022-04-11 22:52:37 +0530

    Added : Use class methode and variables to calculate employee wahes

---
+++ bridgeLabz_day8/EmployeeWage.java
@@ -0,0 +1,66 @@
+package com.bridgeLabz_day8;
+/*  In this program calculate employee wage by using the class methode and class variables.
+ * 
+ * @ Dnyandeo Bansode
+ * 
+ */
+public class EmployeeWage {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_WAGE_PER_HR = 20;
+	public static final int MAX_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	
+	public static void calculateEmployeeWage() {
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 1;
+		int countFullTime = 0;
+		int countPartTime = 0;
+		int countAbsent = 0;
+		/* Here is generate random num rs 0 or 1 or 2 .
+		 * using switch case calculate monthly employee wage of employees.
+		 */
+		while ((totalEmpHrs <= MAX_HRS_IN_MONTH) && (totalWorkingDays <= MAX_WORKING_DAYS)) {
+			int Emp_Check = (int) Math.floor(Math.random() * 10) % 3;
+
+			switch (Emp_Check) {
+			case IS_FULL_TIME:
+				System.out.println("Employee Present : Full Time");
+				empHrs = 8;
+				countFullTime++;
+				break;
+
+			case IS_PART_TIME:
+				System.out.println("Employee is Present : Part Time");
+				empHrs = 4;
+				countPartTime++;
+				break;
+
+			default:
+				System.out.println("Employee is Absent");
+				empHrs = 0;
+				countAbsent++;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Employee Wage is : " + empHrs);
+			System.out.println();
+			totalWorkingDays++;
+		}
+		// Calculating Monthly wage
+		System.out.println("Days Employee was present full time : " + countFullTime);
+		System.out.println("Days Employee was present half time : " + countPartTime);
+		System.out.println("Days Employee was absent : " + countAbsent);
+		System.out.println();
+		int totalMonthlyWage = totalEmpHrs * EMP_WAGE_PER_HR;
+		System.out.println("Employee worked " + totalEmpHrs + " Hrs in " + (totalWorkingDays - 1) + " days.");
+		System.out.println();
+		System.out.println("Monthly Wage of employee is : Rs. " + totalMonthlyWage);
+	}
+	public static void main(String[] args) {
+		
+		calculateEmployeeWage();
+
+	}
+
+}
